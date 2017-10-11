84481eb6283bd1964e9066172fbe89377e445109
EstebanGM131190 <ie565892@iteso.mx>
2017-10-10 19:59:51 -0500

    interrupt Enabled done

--- UART.c
+++ UART.c
@@ -1,5 +1,5 @@
 #include "UART.h"
-
+#include "MK64F12.h"
 void UART0_Status_IRQHandler(){
 
 }
@@ -11,6 +11,9 @@ void UART_init(UART_ChannelType uartChannel, uint32 systemClk, UART_BaudRateType
 
 
 void UART0_interruptEnable(UART_ChannelType uartChannel){
+	while(!(UART0->S1 & UART_S1_TC_MASK)){ //checa que ya no este transmitiendo nada
+	       UART0->C2  |= UART_S1_RDRF_MASK;              //activa la interrupcion del receptor
+	}
 
 }
 
